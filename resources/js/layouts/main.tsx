200ae3d6754504332869f81211a1bb5e552a22f7
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-04 23:01:41 -0400

    lint fix

--- resources/js/layouts/main.tsx
+++ resources/js/layouts/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 
-const Main: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
+const Main: React.FC<React.PropsWithChildren> = ({ children }) => (
     <div>
         <Header />
         <main style={{ padding: '1rem' }}>
