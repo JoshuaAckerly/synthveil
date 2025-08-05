e100fbf4d7dd842bc1b7d5ede33cb7b8a94e99ed
Joshua Ackerly <ackerlyj05@gmail.com>
2025-08-05 01:59:51 -0400

    Clean Up & Layout

---
+++ resources/js/components/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => (
+    <header style={{ padding: '1rem', textAlign: 'center', background: '#222', color: '#fff' }}>
+        <h1>SynthVeil</h1>
+        <nav>
+            {/* Add navigation links here if needed */}
+        </nav>
+    </header>
+);
+
+export default Header;
