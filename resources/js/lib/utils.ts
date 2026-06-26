46c396034caa7958a77cb81ab06acc491f8efa61
joshua <ackerlyj05@gmail.com>
2026-06-26 05:02:30 -0400

    chore: add Vitest config, update deps and tooling

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,6 +1,2 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-export function cn(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
-}
+// Re-exported from the shared @gj/utils package.
+export { cn } from '@gj/utils';
